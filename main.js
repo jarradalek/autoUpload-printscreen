ed0fd03a4de05449abc6ce7b62a1aa6f38bf23e8
jarradalek <48004060+jarradalek@users.noreply.github.com>
2022-02-02 23:34:42 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,16 @@
 const open = require('open');
 const screenshot = require('screenshot-desktop')
 const { ImgurClient } = require('imgur');
-const client = new ImgurClient({ clientId: 'xDD' });
+const client = new ImgurClient({ clientId: 'XDD' });
 const gkl = require("node-global-key-listener");
 const keypress = new gkl.GlobalKeyboardListener();
 
 keypress.addListener((key) => {
-    
     if (key.rawKey._nameRaw === 'VK_SNAPSHOT' && key.state === 'UP') {
-        
         screenshot().then(async(img) => {
-            const response = await client.upload({
-                image: img,
-                title: 'Screenshot',
-                description: 'Screenshot using nodeJS',
-            });
+            const response = await client.upload({ image: img });
             open(response.data.link);
-
         })
-    }
-    
+    }   
 });
 process.stdin.setRawMode(true);
